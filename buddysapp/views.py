72099c2224f0c4dea4f719eb2badba2eddedfee8
HSI <HSI@News-MacBook-Air.local>
2018-01-05 16:54:10 -0500

    Signin and out

---
+++ buddysapp/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+
+# Create your views here.
+def home(request):
+    return redirect(dispensary_home)
+
+@login_required(login_url='/dispensary/sign-in/')
+def dispensary_home(request):
+    return render(request, 'dispensary/home.html')
