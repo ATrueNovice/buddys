27aed054f425f6de0520d4839b054b8e84068e4f
HSI <HSI@News-MacBook-Air.local>
2018-01-06 20:34:00 -0500

    home

--- buddysapp/views.py
+++ buddysapp/views.py
@@ -10,7 +10,7 @@ def home(request):
 
 @login_required(login_url='/dispensary/sign-in/')
 def dispensary_home(request):
-    return render(request, 'dispensary/home.html', {})
+    return render(request, 'dispensary/home.html')
 
 def dispensary_sign_up(request):
     user_form = UserForm()
