3609633014d5de5a1d144977906736ab86721133
HSI <HSI@News-MacBook-Air.local>
2018-01-06 20:20:40 -0500

    newtings

--- buddys/wsgi.py
+++ buddys/wsgi.py
@@ -13,7 +13,7 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "buddys.settings")
 
-application = get_wsgi_application()
+application = get_wsgi_application
 
 #USe whitenoise package for statick files on Heroku
 from whitenoise.django import DjangoWhiteNoise
